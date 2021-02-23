5f3cebaecb5db4385f26d06d2394592e7b508bd8
Eden Leung <31346973+edenleung@users.noreply.github.com>
2021-02-23 13:26:55 +0800

    Update HttpHandlerRunner.php

--- src/HttpHandlerRunner.php
+++ src/HttpHandlerRunner.php
@@ -6,6 +6,9 @@ namespace think\Psr7;
 
 use Psr\Http\Message\ResponseInterface;
 use Laminas\HttpHandlerRunner\Emitter\SapiEmitter;
+use think\App;
+use think\Container;
+use think\event\HttpEnd;
 
 class HttpHandlerRunner
 {
@@ -13,8 +16,16 @@ class HttpHandlerRunner
     {
         $handle = new SapiEmitter();
         $handle->emit($response);
+
+        $app = Container::getInstance()->make(App::class);
+        $app->event->trigger(HttpEnd::class, $response);
+        $app->log->save();
         
         // 直接结束 不交给 ThinkPHP Response 处理
+        // public/index.php 以下代码失效 由上方处理代处理
+        // 中间件 end 方法暂时失效
+        // $response->send();
+        // $http->end($response);
         exit();
     }
 }
